ea15a50cbc539833983d2a72017457808108d38e
Abhinandan Jain <55753532+Abhinandan-git@users.noreply.github.com>
2025-08-30 19:02:38 +0530

    Completed NLP (#6)

--- backend/routers/nlp_router.py
+++ backend/routers/nlp_router.py
@@ -3,13 +3,18 @@ from fastapi import APIRouter
 from typing import Dict
 
 # Importing utility functions
-from utils.nlp import convert_to_action
+from utils.nlp import convert_to_action, get_item_id
+from routers.crud_router import delete_item, add_item
 
 router = APIRouter()
 
 # The `cmd` route refers to the text command
 @router.post("/cmd")
-def text_to_action(command: str) -> Dict[str, str | Dict[str, str]]:
+def text_to_action(command: str, user_id: str) -> Dict[str, str | Dict[str, str]]:
 	action = convert_to_action(command)
+
+	item_id = get_item_id(action["payload"])
+
+	delete_item(user_id, item_id) if action["method"] == "DELETE" else add_item(user_id, item_id)
 
 	return {"method": action["method"], "url": action["url"], "payload": action["payload"]}
